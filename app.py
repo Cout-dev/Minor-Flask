dbb9b2a7f4a2ccf5e602363ff2649d62406c0b34
Cout_dev <shubhankarrai007@gmail.com>
2025-02-11 00:55:44 +0530

    cors

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 # Load trained models
 diabetes_model = pickle.load(open("diabetes.pkl", "rb"))
